cf03e9cc816ae1909c2a3a747f5ae1ce31fdcd5b
Cococod3 <greenoliverj@gmail.com>
2022-12-02 13:03:23 +0100

    deleting clean template

--- clean-template/src/App.js
+++
@@ -1,9 +0,0 @@
-// import './App.css';
-function App() {
-  return <div><h1>Hello, world!</h1>
-  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum minus corrupti beatae excepturi molestias. Officiis quo, iure cumque adipisci quibusdam blanditiis molestias. Ut rem consequuntur, iste corporis ea nulla cupiditate mollitia suscipit. Ad dignissimos eum accusantium totam minus. Labore reiciendis temporibus commodi reprehenderit! Vel alias inventore rerum voluptatem soluta atque?
-  </p>
-  </div>;
-}
-
-export default App;
